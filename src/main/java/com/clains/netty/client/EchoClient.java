eb8e2f6d5098851817a3f8b7ec4b0b40139c9d42
王子健 <wangzj>
2021-12-13 15:42:10 +0800

    first comm

---
+++ src/main/java/com/clains/netty/client/EchoClient.java
@@ -0,0 +1,55 @@
+package com.clains.netty.client;
+
+import com.clains.netty.handler.EchoClientHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @ClassName EchoClient
+ * @Author wangzj
+ * @Date 2021/12/9 11:17
+ * @Version V1.0
+ **/
+public class EchoClient {
+
+    private final String host;
+
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        final ChannelHandler channelHandler = new EchoClientHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(channelHandler);
+                        }
+                    });
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoClient("127.0.0.1", 5000).start();
+    }
+
+}
---
+++ src/main/java/com/clains/netty/server/EchoServer.java
@@ -0,0 +1,52 @@
+package com.clains.netty.server;
+
+import com.clains.netty.handler.EchoServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @ClassName EchoServer
+ * @Author wangzj
+ * @Date 2021/12/8 18:01
+ * @Version V1.0
+ **/
+public class EchoServer {
+
+    private final int port;
+
+    public EchoServer(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoServer(5000).start();
+    }
+
+    public void start() throws Exception {
+        final ChannelHandler channelHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(channelHandler);
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+}
